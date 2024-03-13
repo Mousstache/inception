01c7893b14bce5605a300788d258cb65879d8053
Moussa TROIANO <motroian@bess-f2r4s1.clusters.42paris.fr>
2024-03-13 17:44:10 +0100

    inssepchionne

--- Makefile
+++ Makefile
@@ -1,23 +1,40 @@
-all: 
-	mkdir -p /home/motroian/data/mariadb
-	mkdir -p /home/motroian/data/wordpress
-	docker compose -f ./srcs/docker-compose.yml build
-	docker compose -f ./srcs/docker-compose.yml up -d
+# Variables
+DOCKER_COMPOSE_FILE := ./srcs/docker-compose.yml
+DATA_DIR := /home/motroian/data
+MARIADB_DATA_DIR := $(DATA_DIR)/mariadb
+WORDPRESS_DATA_DIR := $(DATA_DIR)/wordpress
 
-logs:
+# Commandes
+up:
+	mkdir -p $(MARIADB_DATA_DIR)
+	mkdir -p $(WORDPRESS_DATA_DIR)
+	docker compose -f $(DOCKER_COMPOSE_FILE) up --build -d
+
+down:
+	docker compose -f $(DOCKER_COMPOSE_FILE) down
+
+clean:
+	docker system prune
+
+show:
+	docker ps
+	docker volume ls -q
+	docker container ls -q
+
+renew:
+	make down
+	-docker volume rm $(DOCKER_COMPOSE_FILE)_mariadb-vol
+	-docker volume rm $(DOCKER_COMPOSE_FILE)_wordpress-vol
+	sudo rm -rf $(DATA_DIR)
+	make up
+
+see:
 	docker logs wordpress
 	docker logs mariadb
 	docker logs nginx
 
-clean:
-	docker container stop nginx mariadb wordpress
-	docker network rm inception
+restart:
+	make down
+	make up
 
-fclean: clean
-	@sudo rm -rf /home/motroian/data/mariadb/*
-	@sudo rm -rf /home/motroian/data/wordpress/*
-	@docker system prune -af
-
-re: fclean all
-
-.Phony: all logs clean fclean
+.PHONY: up down clean show renew see restart
--- srcs/requirements/wordpress/tools/wpscript.sh
+++ srcs/requirements/wordpress/tools/wpscript.sh
@@ -1,47 +1,13 @@
-#!/bin/bash
-#set -eux
+#!/bin/sh
 
-cd /var/www/html/wordpress
-
-if ! wp core is-installed; then
-wp config create	--allow-root --dbname=${SQL_DATABASE} \
-			--dbuser=${SQL_USER} \
-			--dbpass=${SQL_PASSWORD} \
-			--dbhost=${SQL_HOST} \
-			--url=https://${DOMAIN_NAME};
-
-wp core install	--allow-root \
-			--url=https://${DOMAIN_NAME} \
-			--title=${SITE_TITLE} \
-			--admin_user=${ADMIN_USER} \
-			--admin_password=${ADMIN_PASSWORD} \
-			--admin_email=${ADMIN_EMAIL};
-
-wp user create		--allow-root \
-			${USER1_LOGIN} ${USER1_MAIL} \
-			--role=author \
-			--user_pass=${USER1_PASS} ;
-
-wp cache flush --allow-root
-
-# it provides an easy-to-use interface for creating custom contact forms and managing submissions, as well as supporting various anti-spam techniques
-wp plugin install contact-form-7 --activate
-
-# set the site language to English
-wp language core install en_US --activate
-
-# remove default themes and plugins
-wp theme delete twentynineteen twentytwenty
-wp plugin delete hello
-
-# set the permalink structure
-wp rewrite structure '/%postname%/'
-
+if [ -f ./wp-config.php ]
+then
+    echo "Wordpress already installed, skipping download and configuration."
+else
+    wp core download --allow-root \
+    && wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOSTNAME --allow-root \
+    && wp core install --url=https://localhost --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWD --admin_email=$WP_ADMIN_MAIL --allow-root \
+    && wp user create $WP_USER $WP_USER_MAIL --role=author --user_pass=$WP_USER_PASSWD --porcelain --allow-root
 fi
 
-if [ ! -d /run/php ]; then
-	mkdir /run/php;
-fi
-
-# start the PHP FastCGI Process Manager (FPM) for PHP version 7.3 in the foreground
-exec /usr/sbin/php-fpm7.3 -F -R
+/usr/sbin/php-fpm7.3 -F -R
